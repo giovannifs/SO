2d5a8dcbfb79f09865de245247ca7d938dfb48a1
Giovanni <giovanni.farias@ufrpe.br>
2022-12-05 11:46:37 -0300

    Uploading the first codes discussed in the classroom: exemplo_fork and word_count program versions (single and multi processes).

---
+++ fork/exemplo_fork.c
@@ -0,0 +1,29 @@
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char** argv) {
+	char estado = 'p';
+	int my_pid = getpid();
+    	int child_pid;
+
+    	printf("%d: processos antes da criação do processo filho\n", my_pid);
+	system("ps -f");
+    	printf("%d: criando processo filho\n", my_pid);
+	child_pid = fork();
+	if (child_pid == 0) { // codigo a ser executado pelo filho
+        	printf("%d: minha cópia de pid ainda tem o id de meu pai: %d\n", getpid(), my_pid);
+        	my_pid = getpid();
+        	printf("%d: o valor da variável estado no processo filho é: %c\n", my_pid, estado);
+        	printf("%d: filho está aguardando uma entrada do teclado\n", my_pid);
+		estado = getchar();
+	} else { // codigo a ser executado pelo pai
+        	printf("%d: processos depois da criação do processo filho\n", my_pid);
+		system("ps -f");
+        	printf("%d: pai está esperando pelo filho: %d\n", my_pid, child_pid);
+		wait(&child_pid);
+	}
+    	printf("%d: terminando com o valor da variável estado igual a: %c\n", my_pid, estado);
+}
